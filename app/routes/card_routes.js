35446c6665944880e1e95a6bcb4350345c262a70
billrobinson0215 <bilrobinson0215@gmail.com>
2022-01-03 11:22:59 -0500

    final final commit

--- app/routes/card_routes.js
+++ app/routes/card_routes.js
@@ -1,9 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
-const mongoose = require('mongoose')
-
-const requiresToken = passport.authenticate('bearer', { session: false })
 
 // const Collection = require('../models/collection.js')
 const Collection = require('../models/collection.js')
@@ -27,35 +23,35 @@ router.get('/cards/show', (req, res, next) => {
 
 router.get('/cards/:name', (req, res, next) => {
   Card.findOne({name: req.params.name})
-		// respond with status 200 and JSON of the examples
-		.then((cards) => res.status(200).json({ cards }))
-		// if an error occurs, pass it to the handler
-		.catch(next)
+  // respond with status 200 and JSON of the examples
+    .then((cards) => res.status(200).json({ cards }))
+  // if an error occurs, pass it to the handler
+    .catch(next)
 })
 
 router.get('/cardsid/:id', (req, res, next) => {
-	Card.findOne({ _id: req.params.id })
-		// respond with status 200 and JSON of the examples
-		.then((cards) => res.status(200).json({ cards }))
-		// if an error occurs, pass it to the handler
-		.catch(next)
+  Card.findOne({ _id: req.params.id })
+  // respond with status 200 and JSON of the examples
+    .then((cards) => res.status(200).json({ cards }))
+  // if an error occurs, pass it to the handler
+    .catch(next)
 })
 
 router.delete('/cards/delete/:id/:cardId', async (req, res, next) => {
-		const card = await Card.findById(req.params.cardId)
-		console.log(req.params.cardId)
-		console.log(req.params.id)
-		Collection.findById(req.params.id)
-			.then((collection) => {
-				const foundCard = collection.cards.findIndex(cardIndex => {
-          return cardIndex.name === card.name
-        })
-        collection.cards.splice(foundCard, 1)
-				collection.save().then((collection) => {
-					res.status(204).json(collection)
-				})
-			})
-      .catch(next)
+  const card = await Card.findById(req.params.cardId)
+  console.log(req.params.cardId)
+  console.log(req.params.id)
+  Collection.findById(req.params.id)
+    .then((collection) => {
+      const foundCard = collection.cards.findIndex(cardIndex => {
+        return cardIndex.name === card.name
+      })
+      collection.cards.splice(foundCard, 1)
+      collection.save().then((collection) => {
+        res.status(204).json(collection)
+      })
     })
+    .catch(next)
+})
 
 module.exports = router
